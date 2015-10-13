aaac4177102b2de6b4ac904d2594be9e6ff713a4
Lucio Grado <lucio.grado@make-it-app.it>
2015-10-13 16:09:47 +0200

    second commit

--- app/src/main/java/com/martafoderaro/myswiperefresh/MainActivity.java
+++ app/src/main/java/com/martafoderaro/myswiperefresh/MainActivity.java
@@ -22,7 +22,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
